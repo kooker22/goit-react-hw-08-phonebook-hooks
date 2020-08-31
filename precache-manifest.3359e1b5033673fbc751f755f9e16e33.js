2c1acd5f8f34861ba95e12753053e91ac629c471
Иван Попович <kooker22>
2020-08-31 19:08:38 +0300

    Updates

---
+++ precache-manifest.3359e1b5033673fbc751f755f9e16e33.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e09588de9abe56312d3e617a94153e",
+    "url": "/goit-react-hw-08-phonebook-hooks/index.html"
+  },
+  {
+    "revision": "ee55859c8308f5b58682",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/css/3.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "2836ec269a1eb82e9991",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/css/4.93af0dcb.chunk.css"
+  },
+  {
+    "revision": "d4277b54c8f2b590e133",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/css/5.d34346ea.chunk.css"
+  },
+  {
+    "revision": "65bd4a5434d12370323b",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/css/main.f98ad8a7.chunk.css"
+  },
+  {
+    "revision": "0e9f28536b846cacb901",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/0.a127012b.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/0.a127012b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee55859c8308f5b58682",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/3.c28ad96a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/3.c28ad96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2836ec269a1eb82e9991",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/4.9365bfe0.chunk.js"
+  },
+  {
+    "revision": "d4277b54c8f2b590e133",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/5.de66886a.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/5.de66886a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e05a0bbc69d8df26489",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/6.df78a618.chunk.js"
+  },
+  {
+    "revision": "a7e4ef5c45ab3e952848",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/7.1a831b17.chunk.js"
+  },
+  {
+    "revision": "65bd4a5434d12370323b",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/main.b3bcbdaa.chunk.js"
+  },
+  {
+    "revision": "cb118b8212ff92c57dbb",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/js/runtime-main.9a5002b2.js"
+  },
+  {
+    "revision": "ccafcf3449f168838370d2463e7dc6ea",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/media/default-avatar.ccafcf34.png"
+  },
+  {
+    "revision": "2055587d8f977f5aecfa59330f3db138",
+    "url": "/goit-react-hw-08-phonebook-hooks/static/media/icon.2055587d.svg"
+  }
+]);
